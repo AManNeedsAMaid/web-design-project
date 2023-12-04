239ec91fe09b225df1cd21fe9d4749b8820df9d2
amanneedsamaid <github.xfyo6@simplelogin.com>
2023-12-03 21:30:18 -0500

    Clickable dropdown and subdropdown
    Had to add a bit to the Javascript!

--- dropdown.js
+++ dropdown.js
@@ -4,15 +4,29 @@ function myDropdown() {
   document.getElementById("nav-dropdown").classList.toggle("show");
 }
 
+function mySubDropdown() {
+  document.getElementById("nav-subdropdown").classList.toggle("subdrop-show");
+}
+
 // Close the dropdown menu if the user clicks outside of it
 window.onclick = function (event) {
-  if (!event.target.matches(".drop-button")) {
+  if (!event.target.matches([".drop-button", ".subdropdown-button"])) {
     var dropdowns = document.getElementsByClassName("dropdown-content");
     var i;
     for (i = 0; i < dropdowns.length; i++) {
       var openDropdown = dropdowns[i];
       if (openDropdown.classList.contains("show")) {
         openDropdown.classList.remove("show");
+      }
+    }
+    var subDropdowns = document.getElementsByClassName("subdropdown-content");
+    var i;
+    for (i = 0; i < subDropdowns.length; i++) {
+      var openSubDropdown = subDropdowns[i];
+      if (openSubDropdown.classList.contains("subdrop-show")) {
+        setTimeout(function () {
+          openSubDropdown.classList.remove("subdrop-show");
+        }, 1500);
       }
     }
   }
